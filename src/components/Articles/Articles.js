6c2cb848ed447f9c3e06a72be7d2e7b17ea6d9f1
arystannyrahmetov <arystannyrahmetov@gmail.com>
2022-08-08 21:28:26 +0600

    ubuntu dev tested / article component added

--- src/components/Articles/Articles.js
+++ src/components/Articles/Articles.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import TopMenu from "../TopMenu";
+import Article from "./Article";
+import '../../styles/Articles.css';
 
 function Articles() {
     return (
         <div className="Articles">
             <TopMenu/>
             <div className="Articles-content">
-                <h1>Articles</h1>
+                <h1>Статьи</h1>
+                <Article/>
             </div>
         </div>
     );
